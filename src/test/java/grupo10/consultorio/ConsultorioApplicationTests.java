1359ef1d61cc12693fcc298a079eaf99c8eea6e4
dtisoyUbuntu <danieltisoy2001@gmail.com>
2022-09-05 19:02:27 -0500

    Add java application tests

---
+++ src/test/java/grupo10/consultorio/ConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo10.consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
