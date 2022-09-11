64cb838b473e86a7bd571d9dcd1103b6eedecd3c
dtisoyUbuntu <danieltisoy2001@gmail.com>
2022-09-11 16:18:22 -0500

    fix rename more descriptible variables

--- src/main/java/grupo10/consultorio/modelos/Cita.java
+++ src/main/java/grupo10/consultorio/modelos/Cita.java
@@ -12,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -21,6 +20,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "cita")
+//@JsonIgnoreProperties(value = {"paciente"}, allowGetters = true)
 public class Cita implements Serializable {
 
     @Id
@@ -29,29 +29,25 @@ public class Cita implements Serializable {
     private int idCita;
     @ManyToOne
     @JoinColumn(name = "id_paciente")
-    private Paciente idpaciente;
+    private Paciente paciente;
     @Column(name = "fecha")
     private String fecha;
     @Column(name = "lugar")
     private String lugar;
-    @OneToMany
-    @JoinColumn(name = "estado")
+    @ManyToOne
+    @JoinColumn(name = "id_estado")
     private Estado estado;
 
     public int getIdCita() {
         return idCita;
     }
 
-    public void setIdCita(int idCita) {
-        this.idCita = idCita;
+    public Paciente getPaciente() {
+        return paciente;
     }
 
-    public Paciente getIdpaciente() {
-        return idpaciente;
-    }
-
-    public void setIdpaciente(Paciente idpaciente) {
-        this.idpaciente = idpaciente;
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
     }
 
     public String getFecha() {
